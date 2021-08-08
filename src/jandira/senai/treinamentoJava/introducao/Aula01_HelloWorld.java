8afe0e9b31c6fc80657f2bd64de54ca2692dcb2e
GustavodspHub <77053633+GustavodspHub@users.noreply.github.com>
2021-08-08 00:33:58 -0300

    Studying Java

---
+++ src/jandira/senai/treinamentoJava/introducao/Aula01_HelloWorld.java
@@ -0,0 +1,7 @@
+package jandira.senai.treinamentoJava.introducao;
+
+public class Aula01_HelloWorld {
+    public static void main (String [] args){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/jandira/senai/treinamentoJava/introducao/Aula02_TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package jandira.senai.treinamentoJava.introducao;
+
+public class Aula02_TiposPrimitivos {
+    public static void main (String [] args){
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        System.out.println("A idade é " + idade + " anos");
+    }
+}
